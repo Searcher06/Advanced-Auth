93bb4c9c0e6fc7b5f442933670f29ccb1a3aaf2e
Ahmad Ibrahim <ahmadibrahimsearcher@gmail.com>
2026-02-20 22:51:45 +0100

    setting up nodemailer

--- src/controllers/auth/auth.schema.ts
+++ src/controllers/auth/auth.schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const registerSchema = z.object({
-  email: z.email(),
+  email: z.email({ error: "Invalid email address" }),
   password: z.string().min(6),
   name: z.string().min(3),
 });
